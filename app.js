c7a899f2f11c75c28b144ac3af125f8e778b451e
yuasa <y.kyuri2014@gmail.com>
2023-06-29 10:04:03 +0900

    減少率順位実装

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ rl.on("close", () => {
   }
   // console.log(prefectureDataMap);
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2)=>{
-    return pair2[1].change - pair1[1].change;
+    return -(pair2[1].change - pair1[1].change);
   });
   // console.log(rankingArray);
-  const rankingStrings = rankingArray.map(([key, value])=>{
-    return `${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
+  const rankingStrings = rankingArray.map(([key, value], i)=>{
+    return `${key}: ${i+1}位 ${value.before}=>${value.after} 変化率: ${value.change}`;
   });
   console.log(rankingStrings);
 }); // 全ての行を読み込んだら"close"イベントが発生
